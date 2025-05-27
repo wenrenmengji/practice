a0f7fafff0c4de8c555cea9118be84bac1c9c239
wenrenmengji <dong288288@outlook.com>
2025-05-27 17:37:54 +0800

    first time using JS

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -1,2 +1,30 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello,world";
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/googlecat.jpg") {
+    myImage.setAttribute("src", "images/googlecat1.jpg");
+  } else {
+    myImage.setAttribute("src", "images/googlecat.jpg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = function () {
+  setUserName();
+};
