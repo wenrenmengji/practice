ea8a740b54f193c6efcc3027262ac4966a6f9bb7
wenrenmengji <dong288288@outlook.com>
2025-06-08 17:53:26 +0800

    form

---
+++ forms_and_buttons/javascript.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  btn.textContent = "YOU CLICKED ME!! ❤️";
+  setTimeout(() => {
+    btn.textContent = "Press me";
+  }, 1000);
+});
